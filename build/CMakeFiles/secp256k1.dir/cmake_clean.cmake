0fd57f60e38a4cd2161d7344e41f312b5be81ece
neels99 <vasile.danil1@gmail.com>
2021-04-22 09:34:26 +0300

    WORKEDgit add .git add .git add .!

---
+++ build/CMakeFiles/secp256k1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "../libs/lib/libsecp256k1.a"
+  "../libs/src/secp256k1-stamp/secp256k1-build"
+  "../libs/src/secp256k1-stamp/secp256k1-configure"
+  "../libs/src/secp256k1-stamp/secp256k1-download"
+  "../libs/src/secp256k1-stamp/secp256k1-install"
+  "../libs/src/secp256k1-stamp/secp256k1-mkdir"
+  "../libs/src/secp256k1-stamp/secp256k1-patch"
+  "../libs/src/secp256k1-stamp/secp256k1-update"
+  "CMakeFiles/secp256k1"
+  "CMakeFiles/secp256k1-complete"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/secp256k1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "secp256k1.h"
+
+#include <secp256k1.h>
+#include <secp256k1_ecdh.h>
+#include <secp256k1_recovery.h>
+#include <secp256k1_sha256.h>
+#include <memory>
+using namespace std;
+
+secp256k1_context const* getCtx()
+{
+    static std::unique_ptr<secp256k1_context, decltype(&secp256k1_context_destroy)> s_ctx{
+        secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY),
+        &secp256k1_context_destroy
+    };
+    return s_ctx.get();
+}
+
+int main(){
+    secp256k1_pubkey rawPubkey;
+    auto s = getCtx();
+    cout << s << endl;
+}
